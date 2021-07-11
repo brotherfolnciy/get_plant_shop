292252aa87702a05c23d62a2c843d10166093e25
Alibek Gadzhibaev <agadzhibaev@gmail.com>
2021-07-11 04:35:04 +0300

    set transparent SysNavBar color

--- android/app/src/main/kotlin/com/example/plant_shop/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/plant_shop/MainActivity.kt
@@ -1,6 +1,17 @@
 package com.example.plant_shop
 
+import android.os.Build
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.navigationBarColor = 0 
+            window.navigationBarDividerColor = 0
+            window.statusBarColor = 0
+        }
+    }
 }
